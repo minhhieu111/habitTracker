cb5a3b74dac5b9918de936acd45a90e53da1b4db
minhhieu111 <leminhhieu12112003@gmail.com>
2025-05-25 01:40:54 +0700

    feature: transfer field(difficulty,repeatFrequency,repeatEvery,repeatDays,repeatMonthDays) Daily->UserDaily, field(difficulty) Habit->UserHabit, add UserAchievement class

--- src/main/java/com/example/habittracker/Domain/Daily.java
+++ src/main/java/com/example/habittracker/Domain/Daily.java
@@ -4,9 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Getter
@@ -20,36 +18,10 @@ public class Daily {
     private Long DailyId;
     private String title;
     private String description;
-    @Enumerated(EnumType.STRING)
-    private Difficulty difficulty;
-    @Enumerated(EnumType.STRING)
-    private RepeatFrequency repeatFrequency;
     private LocalDate createAt = LocalDate.now();
-    private Integer repeatEvery;
     private Long challengeId;
-
-    @ElementCollection
-    @CollectionTable(name = "daily_repeat_days", joinColumns = @JoinColumn(name = "daily_id"))
-    @Column(name = "day")
-    @Enumerated(EnumType.STRING)
-    private Set<DayOfWeek> repeatDays;
-
-    @ElementCollection
-    @CollectionTable(name = "user_daily_repeat_month_days", joinColumns = @JoinColumn(name = "user_daily_id"))
-    @Column(name = "day_of_month")
-    private Set<Integer> repeatMonthDays;
 
     @OneToMany(mappedBy = "daily")
     private List<UserDaily> userDailies;
 
-
-    public enum Difficulty {
-        EASY, MEDIUM, HARD
-    }
-    public enum RepeatFrequency {
-        DAILY, WEEKLY, MONTHLY
-    }
-    public enum DayOfWeek {
-        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
-    }
 }
--- src/main/java/com/example/habittracker/Repository/UserDailyRepository.java
+++ src/main/java/com/example/habittracker/Repository/UserDailyRepository.java
@@ -1,16 +1,18 @@
 package com.example.habittracker.Repository;
 
 import com.example.habittracker.Domain.Daily;
-import com.example.habittracker.Domain.Diary;
 import com.example.habittracker.Domain.User;
 import com.example.habittracker.Domain.UserDaily;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserDailyRepository extends JpaRepository<UserDaily,Long> {
-    UserDaily findByUserAndDaily(User user, Daily daily);
+    Optional<UserDaily> findByUserAndDaily(User user, Daily daily);
+
+
+    List<UserDaily> findByUser(User user);
 }
